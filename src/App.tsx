c3536dc37abc2b10583ec5674081bd784602c13e
Mike Spadafora <148890405+mspadafora@users.noreply.github.com>
2025-05-09 19:26:25 +0000

    Set starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 export const App = () => {
-  return <div className="font-medium">hello</div>
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-3">
+      <h1 className="text-3xl font-semibold text-center">
+        Interactive Intro to React
+      </h1>
+      To Begin, start writing code in this div element!
+    </div>
+  )
 }
